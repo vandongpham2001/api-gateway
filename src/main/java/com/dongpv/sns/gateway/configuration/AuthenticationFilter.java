23916d868ef39f1ec0c53dc918c0ecab85114d3e
DongPV <vandongpham2001@gmail.com>
2025-10-21 18:45:39 +0700

    chore: update build configuration and logging setup, add pre-commit hook and properties file

--- src/main/java/com/dongpv/sns/gateway/configuration/AuthenticationFilter.java
+++ src/main/java/com/dongpv/sns/gateway/configuration/AuthenticationFilter.java
@@ -1,14 +1,8 @@
 package com.dongpv.sns.gateway.configuration;
 
-import com.dongpv.sns.gateway.dto.ApiResponse;
-import com.dongpv.sns.gateway.service.IdentityService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.FieldDefaults;
-import lombok.experimental.NonFinal;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
@@ -21,10 +15,18 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
 
-import java.util.Arrays;
-import java.util.List;
+import com.dongpv.sns.gateway.dto.ApiResponse;
+import com.dongpv.sns.gateway.service.IdentityService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
@@ -36,10 +38,7 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
 
     @NonFinal
     private String[] publicEndpoints = {
-            "/identity/auth/.*",
-            "/identity/users/registration",
-            "/notification/email/send",
-            "/file/media/download/.*"
+        "/identity/auth/.*", "/identity/users/registration", "/notification/email/send", "/file/media/download/.*"
     };
 
     @Value("${app.api-prefix}")
@@ -48,25 +47,24 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("Enter authentication filter....");
+        LOGGER.info("Enter authentication filter....");
 
-        if (isPublicEndpoint(exchange.getRequest()))
-            return chain.filter(exchange);
+        if (isPublicEndpoint(exchange.getRequest())) return chain.filter(exchange);
 
         // Get token from authorization header
         List<String> authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
-        if (CollectionUtils.isEmpty(authHeader))
-            return unauthenticated(exchange.getResponse());
+        if (CollectionUtils.isEmpty(authHeader)) return unauthenticated(exchange.getResponse());
 
         String token = authHeader.getFirst().replace("Bearer ", "");
-        log.info("Token: {}", token);
+        LOGGER.info("Token: {}", token);
 
-        return identityService.introspect(token).flatMap(introspectResponse -> {
-            if (introspectResponse.getData().isValid())
-                return chain.filter(exchange);
-            else
-                return unauthenticated(exchange.getResponse());
-        }).onErrorResume(throwable -> unauthenticated(exchange.getResponse()));
+        return identityService
+                .introspect(token)
+                .flatMap(introspectResponse -> {
+                    if (introspectResponse.getData().isValid()) return chain.filter(exchange);
+                    else return unauthenticated(exchange.getResponse());
+                })
+                .onErrorResume(throwable -> unauthenticated(exchange.getResponse()));
     }
 
     @Override
@@ -74,16 +72,14 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
         return -1;
     }
 
-    private boolean isPublicEndpoint(ServerHttpRequest request){
+    private boolean isPublicEndpoint(ServerHttpRequest request) {
         return Arrays.stream(publicEndpoints)
                 .anyMatch(s -> request.getURI().getPath().matches(apiPrefix + s));
     }
 
-    Mono<Void> unauthenticated(ServerHttpResponse response){
-        ApiResponse<?> apiResponse = ApiResponse.builder()
-                .code(1401)
-                .message("Unauthenticated")
-                .build();
+    Mono<Void> unauthenticated(ServerHttpResponse response) {
+        ApiResponse<?> apiResponse =
+                ApiResponse.builder().code(1401).message("Unauthenticated").build();
 
         String body = null;
         try {
@@ -95,7 +91,6 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
         response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 
-        return response.writeWith(
-                Mono.just(response.bufferFactory().wrap(body.getBytes())));
+        return response.writeWith(Mono.just(response.bufferFactory().wrap(body.getBytes())));
     }
 }
